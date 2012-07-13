d76a52e66d6b0063a4d89f20c4d5d4fbcb32336a
Mark Pleskac <markpleskac@gmail.com>
2012-07-12 19:49:27 -0700

    initial 2

---
+++ TicTacToe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TicTacToe
+//
+//  Created by Mark Pleskac on 7/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
